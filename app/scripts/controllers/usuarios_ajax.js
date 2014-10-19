0dc8eb64c72f69c42d10012ca0e4f6a9b9f859eb
Fabio Rodrigues <fabiorodriguesba@gmail.com>
2014-10-19 14:36:45 -0300

    Criação de botão de status

--- app/scripts/controllers/usuarios_ajax.js
+++ app/scripts/controllers/usuarios_ajax.js
@@ -16,6 +16,7 @@ angular.module('firstApp')
     var pageRecords = 3;
     var qtdPages = 0;
 
+
     $scope.UsuariosAjax = [];
     $scope.pages = new Array();
     $scope.form = "insert";
@@ -29,32 +30,39 @@ angular.module('firstApp')
 
             // Calcular e preecher as páginas
             var total = data.Usuarios.length;
+            $scope.Usuarios = data.Usuarios;
             qtdPages = Math.ceil(total/pageRecords); //.ceil é para arredondar qtd pra o inteiro superior
             for (var i = 1; i <= qtdPages; i++) {
                 $scope.pages.push(i);
             }
 
-            $scope.UsuariosAjax = data.Usuarios;
+            $scope.Usuarios = data.Usuarios;
+            $scope.UsuariosAjax = $scope.Usuarios;
+            /*$scope.UsuariosTemp = data.Usuarios;*/
             $scope.paginate(1);
+            /*console.log($scope.UsuariosTemp);*/
         })
         .error(function(data, status, headers, config) {
-            //alguma erro ocorreu :(
+            //algum erro ocorreu :(
         });
 
     $scope.insert = function() {
         var novoUsuario = {
             id: $scope.id,
             nome: $scope.nome,
-            data: $scope.data
+            data: $scope.data,
+            status: $scope.status
         };
-        //push insere item
-        $scope.Usuarios.push(novoUsuario)
+        //push insere item na variavel de escopo
+        $scope.Usuarios.push(novoUsuario);
+        $scope.paginate(1);
         $scope.cancel();
     }
     $scope.update = function() {
         $scope.Usuarios[$scope.index].data = $scope.data;
         $scope.Usuarios[$scope.index].nome = $scope.nome;
         $scope.Usuarios[$scope.index].id = $scope.id;
+        $scope.Usuarios[$scope.index].status = $scope.status;
         $scope.cancel();
     }
     $scope.cancel = function() {
@@ -63,25 +71,69 @@ angular.module('firstApp')
         $scope.id = "";
         $scope.nome = "";
         $scope.data = "";
+        $scope.status = "";
+        $scope.btnReset();
+        
     };
 
 
     $scope.delete = function(index) {
         //splice remove item
         $scope.Usuarios.splice(index, 1);
+        $scope.cancel();
     }
+
     $scope.edit = function(index) {
+
         $scope.index = index;
         $scope.form = "update";
         $scope.id = $scope.Usuarios[index].id;
         $scope.nome = $scope.Usuarios[index].nome;
         $scope.data = $scope.Usuarios[index].data;
+        $scope.status = $scope.Usuarios[index].status;
+
+        $scope.btnReset();
+
+        switch ($scope.status){
+        case "Ativo":
+            $scope.btnAtivo = "btn btn-success";        
+            $scope.btnInativo = "btn btn-default";    
+        break;
+        case "Inativo":
+            $scope.btnAtivo = "btn btn-default";
+            $scope.btnInativo = "btn btn-danger";    
+        break;
+        }
+
     };
 
 
     $scope.paginate = function (page) {
         var coeficiente = ((page-1)*pageRecords);
+
         $scope.Usuarios = $scope.UsuariosAjax.slice(coeficiente,coeficiente+pageRecords);
+        $scope.cancel();
     }
+
+
+    $scope.btnReset = function() {
+        $scope.btnAtivo = "btn btn-default";        
+        $scope.btnInativo = "btn btn-default";         
+    }
+
+    $scope.setAtivo = function (index){
+        $scope.status = "Ativo";
+        $scope.btnAtivo = "btn btn-success";
+        $scope.btnInativo = "btn btn-default";
+        
+    }
+    $scope.setInativo = function (index){
+        $scope.status = "Inativo";
+        $scope.btnAtivo = "btn btn-default";
+        $scope.btnInativo = "btn btn-danger";
+        
+    }
+
+
 
 });
